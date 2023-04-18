fa90c2c51ec058d9b740502763f0e1770613dfae
mariana-cunha <mari.marques26@gmail.com>
2023-04-18 13:45:05 -0300

    altera bio e add link do github

--- src/App.js
+++ src/App.js
@@ -19,10 +19,11 @@ function App() {
     <div className="redes-sociais">
       <a href="mailto:mari.marques26@gmail.com"><i>mari.marques26@gmail.com</i></a>
       <a href="https://www.linkedin.com/in/mariana--cunha/" target="_blank" rel="noopener noreferrer"><i>linkedin</i></a>
+      <a href="https://github.com/mariana-cunha" target="_blank" rel="noopener noreferrer"><i>github</i></a>
     </div>
         </div>
         <div className="descricao">
-          <p>Minha trajetória é de fato única. Trabalho na <a href="https://www.thoughtworks.com/pt-br" target="_blank" rel="noopener noreferrer"> <b>Thoughtworks Brasil</b></a> como consultora de software front-end desde 2021. Antes, estive no <a target="_blank" rel="noopener noreferrer" href="https://www.estadao.com.br/infograficos"><b>Estadão</b></a> desenvolvendo histórias que combinavam informação, design e programação, além de oferecer suporte à redação com layout e componentes interativos em especiais. Passei um curto período também na <a target="_blank" rel="noopener noreferrer" href="https://www.newton.ag/"><b>Newton Agência</b></a>.
+          <p>Desenvolvedora front-end, jornalista. Nascida em Santos. Trabalhei na <a href="https://www.thoughtworks.com/pt-br" target="_blank" rel="noopener noreferrer"> <b>Thoughtworks Brasil</b></a> como consultora de software front-end por quase dois anos. Antes, estive no <a target="_blank" rel="noopener noreferrer" href="https://www.estadao.com.br/infograficos"><b>Estadão</b></a> desenvolvendo histórias que combinavam informação, design e programação, além de oferecer suporte à redação com layout e componentes interativos em especiais. Passei um curto período também na <a target="_blank" rel="noopener noreferrer" href="https://www.newton.ag/"><b>Newton Agência</b></a>.
           </p>
           <p>Clique nos cards abaixo para conhecer o meu trabalho. Adicionei um ícone <FontAwesomeIcon size="1x" alt="ícone de premiação" icon={faAward}
             /> nos itens que tive reconhecimento em alguma premiação. É possível que alguns links estejam disponíveis apenas para assinantes do Estadão, <del>infelizmente</del>.
