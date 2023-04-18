5f0abff1b391ffb067dda9410c1e72c13a6cfd09
mariana-cunha <mari.marques26@gmail.com>
2023-04-18 14:19:00 -0300

    add pdf e altera bio

--- src/App.js
+++ src/App.js
@@ -15,27 +15,23 @@ function App() {
           />
           <h1>Mariana Cunha</h1>
           <p>Front-end dev, jornalista</p>
-    
-    <div className="redes-sociais">
-      <a href="mailto:mari.marques26@gmail.com"><i>mari.marques26@gmail.com</i></a>
-      <a href="https://www.linkedin.com/in/mariana--cunha/" target="_blank" rel="noopener noreferrer"><i>linkedin</i></a>
-      <a href="https://github.com/mariana-cunha" target="_blank" rel="noopener noreferrer"><i>github</i></a>
-    </div>
+          <div className="redes-sociais">
+            <a href="mailto:mari.marques26@gmail.com"><i>mari.marques26@gmail.com</i></a>
+            <a href="https://www.linkedin.com/in/mariana--cunha/" target="_blank" rel="noopener noreferrer"><i>linkedin</i></a>
+            <a href="https://github.com/mariana-cunha" target="_blank" rel="noopener noreferrer"><i>github</i></a>
+          </div>
         </div>
         <div className="descricao">
-          <p>Desenvolvedora front-end, jornalista. Nascida em Santos. Trabalhei na <a href="https://www.thoughtworks.com/pt-br" target="_blank" rel="noopener noreferrer"> <b>Thoughtworks Brasil</b></a> como consultora de software front-end por quase dois anos. Antes, estive no <a target="_blank" rel="noopener noreferrer" href="https://www.estadao.com.br/infograficos"><b>Estadão</b></a> desenvolvendo histórias que combinavam informação, design e programação, além de oferecer suporte à redação com layout e componentes interativos em especiais. Passei um curto período também na <a target="_blank" rel="noopener noreferrer" href="https://www.newton.ag/"><b>Newton Agência</b></a>.
+          <p>Trabalhei na <a href="https://www.thoughtworks.com/pt-br" target="_blank" rel="noopener noreferrer"> <b>Thoughtworks Brasil</b></a> como consultora de software front-end por quase dois anos. Antes, estive no <a target="_blank" rel="noopener noreferrer" href="https://www.estadao.com.br/infograficos"><b>Estadão</b></a> desenvolvendo histórias que combinavam informação, design e programação, além de oferecer suporte à redação com layout e componentes interativos em especiais. Passei um curto período também na <a target="_blank" rel="noopener noreferrer" href="https://www.newton.ag/"><b>Newton Agência</b></a>.
           </p>
-          <p>Clique nos cards abaixo para conhecer o meu trabalho. Adicionei um ícone <FontAwesomeIcon size="1x" alt="ícone de premiação" icon={faAward}
-            /> nos itens que tive reconhecimento em alguma premiação. É possível que alguns links estejam disponíveis apenas para assinantes do Estadão, <del>infelizmente</del>.
+          <p>Clique nos cards abaixo para conhecer os meus trabalhos publicados. Adicionei um ícone <FontAwesomeIcon size="1x" alt="ícone de premiação" icon={faAward}
+            /> nos itens que tive reconhecimento em alguma premiação. É possível que alguns links estejam disponíveis apenas para assinantes do Estadão, <del>infelizmente</del>. Também tenho o <a href="./Mariana-Cunha-Frontend.pdf" target="_blank" rel="noopener noreferrer">currículo em PDF</a>, se precisar. 
           </p>
         </div>
-       
       </section>
-
       <section className="cards">
         <Trampos />
       </section>
-
       <footer>
         Mariana Cunha ≈ <a href="https://youtu.be/vXskkLrIWkE" target="_blank" rel="noopener noreferrer">vivendo a utopia de um Brasil feliz</a> em 2023
       </footer>
