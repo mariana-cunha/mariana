6a0043bb68b84024c40eb03823871900d31c7f4c
Mariana Cunha <mari.marques26@gmail.com>
2025-07-01 15:41:46 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,10 +22,9 @@ function App() {
           </div>
         </div>
         <div className="descricao">
-          <p>Trabalhei na <a href="https://www.thoughtworks.com/pt-br" target="_blank" rel="noopener noreferrer"> <b>Thoughtworks Brasil</b></a> como consultora de software front-end por quase dois anos. Antes, estive no <a target="_blank" rel="noopener noreferrer" href="https://www.estadao.com.br/infograficos"><b>Estadão</b></a> desenvolvendo histórias que combinavam informação, design e programação, além de oferecer suporte à redação com layout e componentes interativos em especiais. Passei um curto período também na <a target="_blank" rel="noopener noreferrer" href="https://www.newton.ag/"><b>Newton Agência</b></a>.
-          </p>
+          <p>Sou front-end jornalista ou jornalista front-end, como queira. Gosto de atuar em projetos que possam combinar informação, design e programação. Além do jornalismo, trabalhei nos setores financeiro, publicitário e consultoria.</p>
           <p>Clique nos cards abaixo para conhecer os meus trabalhos publicados. Adicionei um ícone <FontAwesomeIcon size="1x" alt="ícone de premiação" icon={faAward}
-            /> nos itens que tive reconhecimento em alguma premiação. É possível que alguns links estejam disponíveis apenas para assinantes do Estadão, <del>infelizmente</del>. Também tenho o <a href="./Currículo-Mariana-Cunha-(front-end).pdf" target="_blank" rel="noopener noreferrer">currículo em PDF</a>, se precisar. 
+            /> nos itens que tive reconhecimento em alguma premiação. É possível que alguns links estejam disponíveis apenas para assinantes do Estadão. Também tenho o <a href="./Currículo-Mariana-Cunha-(front-end).pdf" target="_blank" rel="noopener noreferrer">currículo em PDF</a>, se precisar. 
           </p>
         </div>
       </section>
