786079f4c2683c28a910cb9e5b9c9063b6a0d09f
Mariana Cunha <mari.marques26@gmail.com>
2025-08-07 11:01:14 -0300

    fix: change cv pdf, update date

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
             alt="ícone que representa a Mariana, uma mulher branca de cabelos cacheados curtos, fundo lilás, desenhado pela artista Momorsa"
           />
           <h1>Mariana Cunha</h1>
-          <p>Front-end dev, jornalista</p>
+          <p>front-end developer, jornalista</p>
           <div className="redes-sociais">
             <a href="mailto:mari.marques26@gmail.com"><i>mari.marques26@gmail.com</i></a>
             <a href="https://www.linkedin.com/in/mariana--cunha/" target="_blank" rel="noopener noreferrer"><i>linkedin</i></a>
@@ -22,9 +22,9 @@ function App() {
           </div>
         </div>
         <div className="descricao">
-          <p>Sou front-end jornalista ou jornalista front-end, como queira. Gosto de atuar em projetos que possam combinar informação, design e programação. Além do jornalismo, trabalhei nos setores financeiro, publicitário e consultoria.</p>
+          <p>Gosto de atuar em projetos que possam combinar informação, design e programação. Além do jornalismo, trabalhei como consultora de software e </p>
           <p>Clique nos cards abaixo para conhecer os meus trabalhos publicados. Adicionei um ícone <FontAwesomeIcon size="1x" alt="ícone de premiação" icon={faAward}
-            /> nos itens que tive reconhecimento em alguma premiação. É possível que alguns links estejam disponíveis apenas para assinantes do Estadão. Também tenho o <a href="./curriculo-2025.pdf" target="_blank" rel="noopener noreferrer">currículo em PDF</a>, se precisar. 
+            /> nos itens que tive reconhecimento em alguma premiação. É possível que alguns links estejam disponíveis apenas para assinantes do Estadão. Também tenho o <a href="./curriculo-2025.pdf" target="_blank" rel="noopener noreferrer">currículo em PDF</a>, se precisar.
           </p>
         </div>
       </section>
@@ -32,7 +32,7 @@ function App() {
         <Trampos />
       </section>
       <footer>
-        Mariana Cunha ≈ <a href="https://youtu.be/vXskkLrIWkE" target="_blank" rel="noopener noreferrer">vivendo a utopia de um Brasil feliz</a> em 2023
+        Mariana Cunha ≈ <a href="https://youtu.be/vXskkLrIWkE" target="_blank" rel="noopener noreferrer">vivendo a utopia de um Brasil feliz</a> em 2025
       </footer>
     </div>
   );
