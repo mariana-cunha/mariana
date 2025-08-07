a903cd4e4cdc31203a3a9a53978428c23f15931a
Mariana Cunha <mari.marques26@gmail.com>
2025-08-07 17:12:55 -0300

    fix: small adjustments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Styles.scss";
 import { Trampos } from "./Trampos";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAward } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faAward } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
@@ -13,20 +13,23 @@ function App() {
             src="./icon.png"
             alt="ícone que representa a Mariana, uma mulher branca de cabelos cacheados curtos, fundo lilás, desenhado pela artista Momorsa"
           />
-          <h1>Mariana Cunha</h1>
-          <p>front-end developer, jornalista</p>
-          <div className="redes-sociais">
-            <a href="mailto:mari.marques26@gmail.com"><i>mari.marques26@gmail.com</i></a>
-            <a href="https://www.linkedin.com/in/mariana--cunha/" target="_blank" rel="noopener noreferrer"><i>linkedin</i></a>
-            <a href="https://github.com/mariana-cunha" target="_blank" rel="noopener noreferrer"><i>github</i></a>
+          <div>
+            <h1>Mariana Cunha</h1>
+            <p>front-end developer, jornalista</p>
+            <div className="redes-sociais">
+              <a href="mailto:mari.marques26@gmail.com"><i>mari.marques26@gmail.com</i></a>
+              <a href="https://www.linkedin.com/in/mariana--cunha/" target="_blank" rel="noopener noreferrer"><i>linkedin</i></a>
+              <a href="https://github.com/mariana-cunha" target="_blank" rel="noopener noreferrer"><i>github</i></a>
+              <a href="./curriculo-2025.pdf" target="_blank" rel="noopener noreferrer"><i>cv</i></a>
+            </div>
           </div>
         </div>
-        <div className="descricao">
-          <p>Gosto de atuar em projetos que possam combinar <b>informação</b>, <b>design</b> e <b>programação</b>.</p>
+        {/* <div className="descricao">
+          <p className="bold">informação, design e programação.</p>
           <p>Clique nos cards abaixo para conhecer os meus trabalhos publicados.</p>
           <p>Adicionei um ícone <FontAwesomeIcon size="1x" alt="ícone de premiação" icon={faAward} /> nos itens que tive reconhecimento em alguma premiação. É possível que alguns links estejam disponíveis apenas para assinantes do Estadão. Também tenho o <a href="./curriculo-2025.pdf" target="_blank" rel="noopener noreferrer">currículo em PDF</a>, se precisar.
           </p>
-        </div>
+        </div> */}
       </section>
       <section className="cards">
         <Trampos />
